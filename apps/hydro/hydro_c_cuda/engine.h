6397e03a99bd4a3bff2b02bd57cf4bbc87f5386f
Samuel K. Gutierrez <samuel@lanl.gov>
2014-06-23 08:36:33 -0600

    Start of repo layout restructuring.
    Just moving things around. No code changes or additions at this point.

--- apps/hydro/hydro_c_cuda/engine.h
+++
@@ -1,8 +0,0 @@
-#ifndef ENGINE_H_INCL
-#define ENGINE_H_INCL
-
-#include "hydro_structs.h"
-
-void engine(hydro_args H);
-
-#endif
--- apps/hydro/hydro_cuda_runvis/engine.h
+++
@@ -1,8 +0,0 @@
-#ifndef ENGINE_H_INCL
-#define ENGINE_H_INCL
-
-#include "hydro_structs.h"
-
-void engine(int argc, char *argv[], hydro_args H);
-
-#endif
