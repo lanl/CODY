f56beff1d5781c2ad253545a2224ad626577971a
Samuel K. Gutierrez <samuel@lanl.gov>
2014-08-22 20:35:04 -0600

    Better spmv launch + RM raw_rect_ptr usage.

--- legion/lgncg/src/comp-spmv.h
+++ legion/lgncg/src/comp-spmv.h
@@ -38,6 +38,16 @@
 
 #include "legion.h"
 
+namespace {
+struct spmvTaskArgs {
+
+    int64_t nCols;
+
+    spmvTaskArgs(int64_t nCols) : nCols(nCols) { ; }
+};
+
+}
+
 /**
  * implements the SParse Matrix Vector multiply functionality.
  */
@@ -62,21 +72,9 @@ spmv(const SparseMatrix &A,
            x.lDom().get_volume() == y.lDom().get_volume());
     // setup per-task args
     ArgumentMap argMap;
-    CGTaskArgs targs;
-    targs.sa = A;
-    targs.va = x;
-    targs.vb = y;
-    for (int i = 0; i < A.vals.lDom().get_volume(); ++i) {
-        targs.sa.vals.sgb = A.vals.sgb()[i];
-        targs.sa.mIdxs.sgb = A.mIdxs.sgb()[i];
-        targs.sa.nzir.sgb = A.nzir.sgb()[i];
-        targs.va.sgb = x.sgb()[i];
-        targs.vb.sgb = y.sgb()[i];
-        argMap.set_point(DomainPoint::from_point<1>(Point<1>(i)),
-                         TaskArgument(&targs, sizeof(targs)));
-    }
+    spmvTaskArgs targs(A.nCols);
     IndexLauncher il(LGNCG_SPMV_TID, A.vals.lDom(),
-                     TaskArgument(NULL, 0), argMap);
+                     TaskArgument(&targs, sizeof(targs)), argMap);
     // A's regions /////////////////////////////////////////////////////////////
     // vals
     il.add_region_requirement(
@@ -88,11 +86,6 @@ spmv(const SparseMatrix &A,
         RegionRequirement(A.mIdxs.lp(), 0, READ_ONLY, EXCLUSIVE, A.mIdxs.lr)
     );
     il.add_field(idx++, A.mIdxs.fid);
-    // nzir
-    il.add_region_requirement(
-        RegionRequirement(A.nzir.lp(), 0, READ_ONLY, EXCLUSIVE, A.nzir.lr)
-    );
-    il.add_field(idx++, A.nzir.fid);
     // x's regions /////////////////////////////////////////////////////////////
     il.add_region_requirement(
         /* notice we are using the entire region here */
@@ -121,78 +114,56 @@ spmvTask(const LegionRuntime::HighLevel::Task *task,
     using namespace LegionRuntime::Accessor;
     using LegionRuntime::Arrays::Rect;
     (void)ctx; (void)lrt;
-    // A (x3), x, b
-    assert(5 == rgns.size());
-    size_t rid = 0;
-    CGTaskArgs targs = *(CGTaskArgs *)task->local_args;
+    static const uint8_t aValsRID  = 0;
+    static const uint8_t aMIdxsRID = 1;
+    static const uint8_t xRID      = 2;
+    static const uint8_t yRID      = 3;
+    // A (x2), x, b
+    assert(4 == rgns.size());
+    const spmvTaskArgs targs = *(spmvTaskArgs *)task->args;
 #if 0 // nice debug
     printf("%d: sub-grid bounds: (%d) to (%d)\n",
             getTaskID(task), rect.lo.x[0], rect.hi.x[0]);
 #endif
     // name the regions
-    const PhysicalRegion &avpr = rgns[rid++];
-    const PhysicalRegion &aipr = rgns[rid++];
-    const PhysicalRegion &azpr = rgns[rid++];
-    const PhysicalRegion &xpr  = rgns[rid++];
-    const PhysicalRegion &ypr  = rgns[rid++];
+    const PhysicalRegion &avpr = rgns[aValsRID];
+    const PhysicalRegion &aipr = rgns[aMIdxsRID];
+    const PhysicalRegion &xpr  = rgns[xRID];
+    const PhysicalRegion &ypr  = rgns[yRID];
     // convenience typedefs
     typedef RegionAccessor<AccessorType::Generic, double>  GDRA;
     typedef RegionAccessor<AccessorType::Generic, int64_t> GLRA;
     typedef RegionAccessor<AccessorType::Generic, uint8_t> GSRA;
     // sparse matrix
-    GDRA av = avpr.get_field_accessor(targs.sa.vals.fid).typeify<double>();
-    GLRA ai = aipr.get_field_accessor(targs.sa.mIdxs.fid).typeify<int64_t>();
-    GSRA az = azpr.get_field_accessor(targs.sa.nzir.fid).typeify<uint8_t>();
+    GDRA av = avpr.get_field_accessor(0).typeify<double>();
+    const Domain aValsDom = lrt->get_index_space_domain(
+        ctx, task->regions[aValsRID].region.get_index_space()
+    );
+    GLRA ai = aipr.get_field_accessor(0).typeify<int64_t>();
+    const Domain aMIdxsDom = lrt->get_index_space_domain(
+        ctx, task->regions[aMIdxsRID].region.get_index_space()
+    );
     // vectors
-    GDRA x = xpr.get_field_accessor(targs.va.fid).typeify<double>();
-    GDRA y = ypr.get_field_accessor(targs.vb.fid).typeify<double>();
-
-    Rect<1> avsr; ByteOffset avOff[1];
-    Rect<1> myGridBounds = targs.sa.vals.sgb;
-    // calculate nRows and nCols for the local subgrid
-    assert(0 == myGridBounds.volume() % targs.sa.nCols);
-    const int64_t lNRows = myGridBounds.volume() / targs.sa.nCols;
-    const int64_t lNCols = targs.sa.nCols;
-    const double *const avp = av.raw_rect_ptr<1>(myGridBounds, avsr, avOff);
-    bool offd = offsetsAreDense<1, double>(myGridBounds, avOff);
-    assert(offd);
-    // remember that vals and mIdxs should be the same size
-    Rect<1> aisr; ByteOffset aiOff[1];
-    const int64_t *const aip = ai.raw_rect_ptr<1>(myGridBounds, aisr, aiOff);
-    offd = offsetsAreDense<1, int64_t>(myGridBounds, aiOff);
-    assert(offd);
-    // nzir is smaller (by a stencil size factor).
-    Rect<1> azsr; ByteOffset azOff[1];
-    myGridBounds = targs.sa.nzir.sgb;
-    const uint8_t *const azp = az.raw_rect_ptr<1>(myGridBounds, azsr, azOff);
-    offd = offsetsAreDense<1, uint8_t>(myGridBounds, azOff);
-    assert(offd);
-    // x
-    Rect<1> xsr; ByteOffset xOff[1];
-    // notice that we aren't using the subgridBounds here -- need all of x
-    myGridBounds = targs.va.bounds;
-    const double *const xp = x.raw_rect_ptr<1>(myGridBounds, xsr, xOff);
-    offd = offsetsAreDense<1, double>(myGridBounds, xOff);
-    assert(offd);
-    // y
-    Rect<1> ysr; ByteOffset yOff[1];
-    myGridBounds = targs.vb.sgb;
-    double *yp = y.raw_rect_ptr<1>(myGridBounds, ysr, yOff);
-    offd = offsetsAreDense<1, double>(myGridBounds, yOff);
-    assert(offd);
+    GDRA x = xpr.get_field_accessor(0).typeify<double>();
+    GDRA y = ypr.get_field_accessor(0).typeify<double>();
+    const Domain yDom = lrt->get_index_space_domain(
+        ctx, task->regions[yRID].region.get_index_space()
+    );
+    //
+    const int64_t lNCols = targs.nCols;
+    const int64_t lNRows = aValsDom.get_rect<1>().volume() / lNCols;
     // now, actually perform the computation
-    for (int64_t i = 0; i < lNRows; ++i) {
+    DomainPoint pir; pir.dim = 1;
+    GenericPointInRectIterator<1> rowItr(yDom.get_rect<1>());
+    GenericPointInRectIterator<1> ciItr(aMIdxsDom.get_rect<1>());
+    for (int64_t i = 0; i < lNRows; ++i, rowItr++) {
         double sum = 0.0;
-        // get to base of next row of values
-        const double *const cVals = (avp + (i * lNCols));
-        // get to base of next row of "real" indices of values
-        const int64_t *const cIndx = (aip + (i * lNCols));
-        // capture how many non-zero values are in this particular row
-        const int64_t cnnz = azp[i];
-        for (int64_t j = 0; j < cnnz; ++j) {
-            sum += cVals[j] * xp[cIndx[j]];
+        for (int64_t j = 0; j < lNCols; ++j, ciItr++) {
+            int64_t index = ai.read(DomainPoint::from_point<1>(ciItr.p));
+            pir.point_data[0] = index;
+            sum += av.read(DomainPoint::from_point<1>(ciItr.p)) * x.read(pir);
         }
-        yp[i] = sum;
+        y.write(DomainPoint::from_point<1>(rowItr.p), sum);
     }
 }
 
