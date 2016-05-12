c3a7b84fe751049f76967fffe5224bcc1d814b27
Samuel K. Gutierrez <samuel@lanl.gov>
2016-05-11 20:29:31 -0600

    Start of HaloExchange kernel.

---
+++ legion/lgncg/redux/spmv/HaloExchange.cc
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2016      Los Alamos National Security, LLC
+ *                         All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * LA-CC 10-123
+ */
+
+/**
+ * A simple example illustrating a halo exchange using control replication. 
+ */
+
+#include <iostream>
+#include <cstdlib>
+
+#include "legion.h"
+
+#include "TaskIDs.h"
+
+using namespace LegionRuntime::HighLevel;
+
+using namespace std;
+
+void
+mainTask(
+    const Task *task,
+    const std::vector<PhysicalRegion> &regions,
+    Context ctx, HighLevelRuntime *runtime
+) {
+
+}
+
+void
+registerTasks(void) {
+    TaskVariantRegistrar tvr(MAIN_TID, "mainTask");
+    tvr.add_constraint(ProcessorConstraint(Processor::LOC_PROC));
+    Runtime::preregister_task_variant<mainTask>(tvr, "mainTask");
+    Runtime::set_top_level_task_id(MAIN_TID);
+}
+
+int
+main(int argc, char **argv)
+{
+    registerTasks();
+    return Runtime::start(argc, argv);
+}
---
+++ legion/lgncg/redux/spmv/TaskIDs.h
@@ -0,0 +1,5 @@
+#pragma once
+
+enum {
+    MAIN_TID
+};
