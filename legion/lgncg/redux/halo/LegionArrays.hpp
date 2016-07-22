0ccf646ab47409ccedb14901ca53abb22104ac0e
Samuel K. Gutierrez <samuel@lanl.gov>
2016-07-22 14:04:18 -0600

    Checkpoint data structure updates.

--- legion/lgncg/redux/halo/LegionArrays.hpp
+++ legion/lgncg/redux/halo/LegionArrays.hpp
@@ -27,6 +27,7 @@
 
 #pragma once
 
+#include "LegionItems.hpp"
 #include "LegionStuff.hpp"
 
 #include <vector>
@@ -66,84 +67,8 @@ private:
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 template<typename TYPE>
-struct LogicalArray {
-    // Number of elements stored in the vector (the entire extent).
-    int64_t length;
-    // Field ID.
-    LegionRuntime::HighLevel::FieldID fid;
-    // The vector rectangle bounds.
-    LegionRuntime::Arrays::Rect<1> bounds;
-    // Logical region that represents array.
-    LegionRuntime::HighLevel::LogicalRegion logicalRegion;
-private:
-    // Index space.
-    LegionRuntime::HighLevel::IndexSpace mIndexSpace;
-    // Field space.
-    LegionRuntime::HighLevel::FieldSpace mFS;
-    // Vector of partition items.
+struct LogicalArray : public LogicalItem<TYPE> {
     std::vector<PVecItem> mPVec;
-    // The following are used for vector equality tests. That is, equality in
-    // the "are these vectors the same from legion's perspective."
-    id_t mIndexSpaceID;
-    //
-    LegionRuntime::HighLevel::FieldSpaceID mFieldSpaceID;
-    //
-    LegionRuntime::HighLevel::RegionTreeID mRTreeID;
-
-public:
-    /**
-     *
-     */
-    void
-    allocate(
-        int64_t nElems,
-        LegionRuntime::HighLevel::Context &ctx,
-        LegionRuntime::HighLevel::HighLevelRuntime *lrt
-    ) {
-        fid = 0;
-        length = nElems;
-        // calculate the size of the logicalRegion vec (inclusive)
-        auto n = length - 1;
-        // vec rect
-        bounds = Rect<1>(Point<1>::ZEROES(), Point<1>(n));
-        // vector domain
-        Domain dom(Domain::from_rect<1>(bounds));
-        // vec index space
-        mIndexSpace = lrt->create_index_space(ctx, dom);
-        // vec field space
-        mFS = lrt->create_field_space(ctx);
-        // vec field allocator
-        FieldAllocator fa = lrt->create_field_allocator(ctx, mFS);
-        // all elements are going to be of size T
-        fa.allocate_field(sizeof(TYPE), fid);
-        // now create the logical region
-        logicalRegion = lrt->create_logical_region(ctx, mIndexSpace, mFS);
-        // stash some info for equality checks
-        mIndexSpaceID = logicalRegion.get_index_space().get_id();
-        mFieldSpaceID = logicalRegion.get_field_space().get_id();
-        mRTreeID      = logicalRegion.get_tree_id();
-        // at this point we don't have a logical partition...  TODO maybe we can
-        // just check if we are partitioned or not and return the correct
-        // handle..?
-    }
-
-    /**
-     * Cleans up and returns all allocated resources.
-     */
-    void
-    deallocate(
-        LegionRuntime::HighLevel::Context &ctx,
-        LegionRuntime::HighLevel::HighLevelRuntime *lrt
-    ) {
-        for (auto i = mPVec.begin(); i != mPVec.end(); i++) {
-            lrt->destroy_logical_partition(ctx, i->lPart);
-        }
-        mPVec.clear();
-        lrt->destroy_logical_region(ctx, logicalRegion);
-        lrt->destroy_field_space(ctx, mFS);
-        lrt->destroy_index_space(ctx, mIndexSpace);
-    }
-
     /**
      * Returns whether or not two LogicalArrays are the same (as far as the
      * Legion RT is concerned).
@@ -209,9 +134,9 @@ public:
         using LegionRuntime::Arrays::Rect;
 
         // For now only allow even partitioning.
-        assert(0 == length % nParts && "Uneven partitioning requested.");
+        assert(0 == this->mLength % nParts && "Uneven partitioning requested.");
         //
-        int64_t inc = length / nParts; // the increment
+        int64_t inc = this->mLength / nParts; // the increment
         Rect<1> colorBounds(Point<1>(0), Point<1>(nParts - 1));
         Domain colorDomain = Domain::from_rect<1>(colorBounds);
         //          +
@@ -239,54 +164,18 @@ public:
             x1 += inc;
         }
         auto iPart = lrt->create_index_partition(
-                         ctx, mIndexSpace,
+                         ctx, this->mIndexSpace,
                          colorDomain, disjointColoring,
                          true /* disjoint */
         );
         // logical partitions
         using LegionRuntime::HighLevel::LogicalPartition;
-        auto lp = lrt->get_logical_partition(ctx, logicalRegion, iPart);
+        auto lp = lrt->get_logical_partition(ctx, this->logicalRegion, iPart);
         // launch domain -- one task per color
         // launch domain
         LegionRuntime::HighLevel::Domain lDom = colorDomain;
         // add to the vector of partitions
         mPVec.push_back(PVecItem(subgridBnds, lDom, lp));
-    }
-
-    /**
-     *
-     */
-    LegionRuntime::HighLevel::PhysicalRegion
-    mapRegion(
-        LegionRuntime::HighLevel::PrivilegeMode privMode,
-        LegionRuntime::HighLevel::CoherenceProperty cohProp,
-        LegionRuntime::HighLevel::Context &ctx,
-        LegionRuntime::HighLevel::HighLevelRuntime *lrt
-    ) const {
-        using namespace LegionRuntime::HighLevel;
-        using namespace LegionRuntime::Accessor;
-        using LegionRuntime::Arrays::Rect;
-        RegionRequirement req(
-            logicalRegion, privMode, cohProp, logicalRegion
-        );
-        req.add_field(fid);
-        InlineLauncher inl(req);
-        PhysicalRegion reg = lrt->map_region(ctx, inl);
-        reg.wait_until_valid();
-        //
-        return reg;
-    }
-
-    /**
-     *
-     */
-    void
-    unmapRegion(
-        LegionRuntime::HighLevel::PhysicalRegion &mappedRegion,
-        LegionRuntime::HighLevel::Context &ctx,
-        LegionRuntime::HighLevel::HighLevelRuntime *lrt
-    ) const {
-        lrt->unmap_region(ctx, mappedRegion);
     }
 };
 
---
+++ legion/lgncg/redux/halo/LegionItems.hpp
@@ -0,0 +1,207 @@
+/*
+ * Copyright (c) 2014-2016 Los Alamos National Security, LLC
+ *                         All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#include "LegionStuff.hpp"
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+template<typename TYPE>
+struct LogicalItem {
+    // Field ID.
+    LegionRuntime::HighLevel::FieldID fid = 0;
+    // The vector rectangle bounds.
+    LegionRuntime::Arrays::Rect<1> bounds;
+    // Logical region that represents array.
+    LegionRuntime::HighLevel::LogicalRegion logicalRegion;
+protected:
+    // Number of elements stored in the vector (the entire extent).
+    int64_t mLength;
+    // Index space.
+    LegionRuntime::HighLevel::IndexSpace mIndexSpace;
+    // Field space.
+    LegionRuntime::HighLevel::FieldSpace mFS;
+    // The following are used for vector equality tests. That is, equality in
+    // the "are these vectors the same from legion's perspective."
+    id_t mIndexSpaceID;
+    //
+    LegionRuntime::HighLevel::FieldSpaceID mFieldSpaceID;
+    //
+    LegionRuntime::HighLevel::RegionTreeID mRTreeID;
+
+public:
+    /**
+     *
+     */
+    void
+    allocate(
+        int64_t nElems,
+        LegionRuntime::HighLevel::Context &ctx,
+        LegionRuntime::HighLevel::HighLevelRuntime *lrt
+    ) {
+        mLength = nElems;
+        // calculate the size of the logicalRegion vec (inclusive)
+        auto n = mLength - 1;
+        // vec rect
+        bounds = Rect<1>(Point<1>::ZEROES(), Point<1>(n));
+        // vector domain
+        Domain dom(Domain::from_rect<1>(bounds));
+        // vec index space
+        mIndexSpace = lrt->create_index_space(ctx, dom);
+        // vec field space
+        mFS = lrt->create_field_space(ctx);
+        // vec field allocator
+        FieldAllocator fa = lrt->create_field_allocator(ctx, mFS);
+        // all elements are going to be of size T
+        fa.allocate_field(sizeof(TYPE), fid);
+        // now create the logical region
+        logicalRegion = lrt->create_logical_region(ctx, mIndexSpace, mFS);
+        // stash some info for equality checks
+        mIndexSpaceID = logicalRegion.get_index_space().get_id();
+        mFieldSpaceID = logicalRegion.get_field_space().get_id();
+        mRTreeID      = logicalRegion.get_tree_id();
+        // at this point we don't have a logical partition...  TODO maybe we can
+        // just check if we are partitioned or not and return the correct
+        // handle..?
+    }
+
+    /**
+     * Cleans up and returns all allocated resources.
+     */
+    void
+    deallocate(
+        LegionRuntime::HighLevel::Context &ctx,
+        LegionRuntime::HighLevel::HighLevelRuntime *lrt
+    ) {
+        lrt->destroy_logical_region(ctx, logicalRegion);
+        lrt->destroy_field_space(ctx, mFS);
+        lrt->destroy_index_space(ctx, mIndexSpace);
+    }
+
+    /**
+     * Returns whether or not two LogicalItems are the same (as far as the
+     * Legion RT is concerned).
+     */
+    static bool
+    same(
+        const LogicalItem &a,
+        const LogicalItem &b
+    ) {
+        return a.mIndexSpaceID == b.mIndexSpaceID &&
+               a.mFieldSpaceID == b.mFieldSpaceID &&
+               a.mRTreeID      == b.mRTreeID;
+    }
+
+    /**
+     *
+     */
+    LegionRuntime::HighLevel::PhysicalRegion
+    mapRegion(
+        LegionRuntime::HighLevel::PrivilegeMode privMode,
+        LegionRuntime::HighLevel::CoherenceProperty cohProp,
+        LegionRuntime::HighLevel::Context &ctx,
+        LegionRuntime::HighLevel::HighLevelRuntime *lrt
+    ) const {
+        using namespace LegionRuntime::HighLevel;
+        using namespace LegionRuntime::Accessor;
+        using LegionRuntime::Arrays::Rect;
+        RegionRequirement req(
+            logicalRegion, privMode, cohProp, logicalRegion
+        );
+        req.add_field(fid);
+        InlineLauncher inl(req);
+        PhysicalRegion reg = lrt->map_region(ctx, inl);
+        reg.wait_until_valid();
+        //
+        return reg;
+    }
+
+    /**
+     *
+     */
+    void
+    unmapRegion(
+        LegionRuntime::HighLevel::PhysicalRegion &mappedRegion,
+        LegionRuntime::HighLevel::Context &ctx,
+        LegionRuntime::HighLevel::HighLevelRuntime *lrt
+    ) const {
+        lrt->unmap_region(ctx, mappedRegion);
+    }
+};
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+template<typename TYPE>
+class PhysicalItem {
+protected:
+    //
+    size_t mLength = 0;
+    //
+    TYPE *mData = nullptr;
+    //
+    PhysicalItem(void) = default;
+public:
+    //
+    PhysicalItem(
+        const PhysicalRegion &physicalRegion,
+        Context ctx,
+        HighLevelRuntime *runtime
+    ) {
+        typedef RegionAccessor<AccessorType::Generic, TYPE>  GRA;
+        GRA tAcc = physicalRegion.get_field_accessor(0).template typeify<TYPE>();
+        //
+        Domain tDom = runtime->get_index_space_domain(
+            ctx, physicalRegion.get_logical_region().get_index_space()
+        );
+        Rect<1> subrect;
+        ByteOffset inOffsets[1];
+        auto subGridBounds = tDom.get_rect<1>();
+        mLength = subGridBounds.volume();
+        //
+        mData = tAcc.template raw_rect_ptr<1>(
+            subGridBounds, subrect, inOffsets
+        );
+        // Sanity.
+        if (!mData || (subrect != subGridBounds) ||
+            !offsetsAreDense<1, TYPE>(subGridBounds, inOffsets)) {
+            // Signifies that something went south.
+            mData = nullptr;
+        }
+        // It's all good...
+    }
+
+    /**
+     *
+     */
+    TYPE *
+    data(void) { return mData; }
+};
