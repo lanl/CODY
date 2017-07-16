8e87291d142f1f3e51f18a2b7143dcd23491c70e
Samuel K. Gutierrez <samuel@lanl.gov>
2017-07-15 19:25:06 -0600

    Silence warning and astyle pass.

--- legion/lgncg/explicit-spmd/YAML_Doc.cc
+++ legion/lgncg/explicit-spmd/YAML_Doc.cc
@@ -28,56 +28,69 @@ using namespace std;
   @param[in] destination_Directory destination directory for the YAML document
   @param[in] destination_FileName file name for the YAML document
 */
-YAML_Doc::YAML_Doc(const std::string & miniApp_Name, const std::string & miniApp_Version, const std::string & destination_Directory, const std::string & destination_FileName) {
-  miniAppName = miniApp_Name;
-  miniAppVersion = miniApp_Version;
-  destinationDirectory = destination_Directory;
-  destinationFileName = destination_FileName;
+YAML_Doc::YAML_Doc(
+    const std::string &miniApp_Name,
+    const std::string &miniApp_Version,
+    const std::string &destination_Directory,
+    const std::string &destination_FileName
+) {
+    miniAppName = miniApp_Name;
+    miniAppVersion = miniApp_Version;
+    destinationDirectory = destination_Directory;
+    destinationFileName = destination_FileName;
 }
 
 //inherits the destructor from YAML_Element
-YAML_Doc::~YAML_Doc(void) {
-}
+YAML_Doc::~YAML_Doc(void) { }
 
 /*!
   Generates YAML from the elements of the document and saves it to a file.
 
   @return returns the complete YAML document as a string
 */
-string YAML_Doc::generateYAML() {
-  string yaml;
+string YAML_Doc::generateYAML(void)
+{
+    string yaml;
 
-  yaml =  yaml + miniAppName + " version: " + miniAppVersion + "\n";
+    yaml =  yaml + miniAppName + " version: " + miniAppVersion + "\n";
 
-  for (size_t i=0; i<children.size(); i++) {
-    yaml = yaml + children[i]->printYAML("");
-  }
+    for (size_t i = 0; i < children.size(); i++) {
+        yaml = yaml + children[i]->printYAML("");
+    }
 
-  time_t rawtime;
-  tm * ptm;
-  time ( &rawtime );
-  ptm = localtime(&rawtime);
-  char sdate[25];
-  //use tm_mon+1 because tm_mon is 0 .. 11 instead of 1 .. 12
-  sprintf (sdate,"%04d.%02d.%02d.%02d.%02d.%02d",ptm->tm_year + 1900, ptm->tm_mon+1,
-      ptm->tm_mday, ptm->tm_hour, ptm->tm_min,ptm->tm_sec);
+    time_t rawtime;
+    tm *ptm;
+    time(&rawtime);
+    ptm = localtime(&rawtime);
+    char sdate[25];
+    //use tm_mon+1 because tm_mon is 0 .. 11 instead of 1 .. 12
+    sprintf(sdate, "%04d.%02d.%02d.%02d.%02d.%02d", ptm->tm_year + 1900, ptm->tm_mon + 1,
+            ptm->tm_mday, ptm->tm_hour, ptm->tm_min, ptm->tm_sec);
 
-  string filename;
-  if (destinationFileName=="")
-    filename = miniAppName + "-" + miniAppVersion + "_";
-  else
-    filename = destinationFileName;
-  filename = filename + string(sdate) + ".yaml";
-  if (destinationDirectory!="" && destinationDirectory!=".") {
-    string mkdir_cmd = "mkdir " + destinationDirectory;
-    system(mkdir_cmd.c_str());
-    filename = destinationDirectory + "/" + destinationFileName;
-  } else
-    filename = "./" + filename;
+    string filename;
+    if (destinationFileName == "") {
+        filename = miniAppName + "-" + miniAppVersion + "_";
+    }
+    else {
+        filename = destinationFileName;
+    }
+    filename = filename + string(sdate) + ".yaml";
+    if (destinationDirectory != "" && destinationDirectory != ".") {
+        string mkdir_cmd = "mkdir " + destinationDirectory;
+        int src = system(mkdir_cmd.c_str());
+        if (src != 0) {
+            std::cerr << "system failure:" << src << std::endl;
+            exit(src);
+        }
+        filename = destinationDirectory + "/" + destinationFileName;
+    }
+    else {
+        filename = "./" + filename;
+    }
 
-  ofstream myfile;
-  myfile.open(filename.c_str());
-  myfile << yaml;
-  myfile.close();
-  return yaml;
+    ofstream myfile;
+    myfile.open(filename.c_str());
+    myfile << yaml;
+    myfile.close();
+    return yaml;
 }
